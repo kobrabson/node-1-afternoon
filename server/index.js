0a3209ffb26c03f3f5029f6450c5c3b03022a2d2
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-27 14:03:00 -0600

    step 2 done

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const products = require('../products.json')
 
 const app = express()
 
 const port = 4000
+
+app.get('/api/products', (req, res) => {
+    res.status(200).send(products)
+})
 
 app.listen(port, () => {
     console.log(`Server listing on port: ${port}`)
